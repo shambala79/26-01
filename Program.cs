340f752f15204967c5ffaa7b7df6b2d5af9f97d3
shambala79 <110595096+shambala79@users.noreply.github.com>
2023-01-27 01:35:31 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,136 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(rows, columns, -100, 100);
+PrintArray(array);
+
+
+
+// -------------------Методы---------------------------------
+double[,] GetArray(int m, int n, int min, int max){
+    double[,] result = new double[m,n];
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            result[i,j] = new Random().NextDouble()*(max+1 - min) + min;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+// Задача 50. Напишите программу, которая на вход принимает число, и проверяет есть ли такое число в 
+// двумерном массиве, а также возвращает сообщение о том, что оно найдено или же указание, что такого
+// элемента нет.
+//Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine()!);
+
+int[,] array1 = GetArray1(3, 4, 0, 10);
+PrintArray1(array1);
+if (Search(array1,x) == true)
+Console.Write($"Число {x} найдено.");
+else
+Console.WriteLine($"Число {x} не найдено.");
+// -------------------Методы---------------------------------
+int[,] GetArray1(int m, int n, int minValue, int maxValue){
+    int[,] result = new int[m,n];
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            result[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray1(int[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool Search (int[,] array, int x){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+      if (array[i,j]==x)
+           return true;                      
+}}
+return false;
+}
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] array2 = GetArray2(3, 4, 0, 10);
+PrintArray2(array2);
+SrArif(array2);
+
+// -------------------Методы---------------------------------
+int[,] GetArray2(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray2(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SrArif(int[,] arrray)
+{
+    int k = array.GetLength(0);
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double avarage = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        { avarage = (avarage + array[i, j]); }
+        avarage = avarage / k;
+        Console.WriteLine("{0:0.##}",avarage);
+    }
+}
